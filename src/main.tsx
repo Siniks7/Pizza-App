8946f801340a3ee0c892f027e72ef4605234e419
siniks7 <siniks7@yandex.ru>
2024-01-06 12:00:30 +0700

    Nedd auth

--- src/main.tsx
+++ src/main.tsx
@@ -10,13 +10,14 @@ import { Product } from './pages/Product/Product.tsx';
 import { AuthLayout } from './layout/Auth/AuthLayout.tsx';
 import { Login } from './pages/Login/Login.tsx';
 import { Register } from './pages/Register/Register.tsx';
+import { RequireAuth } from './helpers/RequireAuth.tsx';
 
 const Menu = lazy(() => import('./pages/Menu/Menu'));
 
 const router = createBrowserRouter([
 	{
 		path: '/',
-		element: <Layout />,
+		element: <RequireAuth><Layout /></RequireAuth>,
 		children: [
 			{
 				path: '/',
