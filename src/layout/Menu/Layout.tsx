030c1e4bdc387fe610aed7e339434a892ca79004
siniks7 <siniks7@yandex.ru>
2024-01-14 01:43:16 +0700

    4363

--- src/layout/Menu/Layout.tsx
+++ src/layout/Menu/Layout.tsx
@@ -43,7 +43,7 @@ export function Layout() {
 						<NavLink to='/cart'  className={({ isActive }) => cn(styles['link'], {
 							[styles.active]: isActive
 						})}>
-							<img src="/cart-icon.svg" alt="Корзина" />Корзина<span className={styles['cart-number']}>{items.reduce((acc, item) => acc += item.count, 0)}</span></NavLink>			
+							<img src="/cart-icon.svg" alt="Корзина" />Корзина<span className={styles['cart-number']}>{items && items.reduce((acc, item) => acc += item.count, 0)}{!items && 0}</span></NavLink>			
 					</div>
 				</div>
 				<Button appearence='small' onClick={logout} className={styles['exit-button']} ><img src="/exit-icon.svg" alt="Выход" />Выйти</Button>
